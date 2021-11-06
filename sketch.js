cc3a240e25739783718aaa17b256b70764e3c279
SofiaRozasNegrao <93043106+SofiaRozasNegrao@users.noreply.github.com>
2021-11-06 11:33:15 -0300

    arrumei as linhas e o trex

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var trex, trexCorrendo
 var chao, chaoImagem
+var invisibleChao
 
 function preload(){
   trexCorrendo = loadAnimation('trex1.png', 'trex3.png', 'trex4.png')
@@ -14,24 +15,24 @@ function setup(){
 
   chao=createSprite(300,190,600,20);
   chao.addImage(chaoImagem);
+  invisibleChao=createSprite(300,199,600,18);
+  invisibleChao.visible=false;
 }
 
 function draw(){
   background("white")
 
   trex.velocityY = trex.velocityY + 0.5;
-  if (keyDown('space')) {
+  if (keyDown('space') &&trex.y>140){
     trex.velocityY = -10
   }
 
   chao.velocityX=-2;
-  console.log(chao.x);
   if(chao.x<0){
     chao.x=chao.width/2;
   }
-  trex.collide(chao);
-  
 
-  
+  trex.collide(invisibleChao);
+
   drawSprites()
 }
